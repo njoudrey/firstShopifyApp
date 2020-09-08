5e57908492015249a0b8a7c7853325241ba09a4a
Nicholas Joudrey <nicholasjoudrey@gmail.com>
2020-09-07 23:05:45 -0400

    Added functionality to display products selected using the resource picker using the ResourceList Polaris feature

--- components/ResourceListWithProducts.js
+++ components/ResourceListWithProducts.js
@@ -1,7 +1,15 @@
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import store from 'store-js';
-import { Card } from '@shopify/polaris'
+import { 
+    Card,
+    DisplayText,
+    Stack,
+    TextStyle,
+    Thumbnail,
+    ResourceList,
+    ResourceItem
+} from '@shopify/polaris'
 
 const GET_PRODUCTS_BY_ID = gql`
     query getProducts($ids: [ID!]!) {
@@ -42,7 +50,48 @@ class ResourceListWithProducts extends React.Component {
                     console.log(data);
                     return (
                         <Card>
-                            <p>Products will go here</p>
+                            <ResourceList
+                                showHeader
+                                resourceName = {{plural: "Products", singular: "Product"}}
+                                items = {data.nodes}
+                                renderItem = {(item) => {
+                                    const media = (
+                                        <Thumbnail
+                                            source = {
+                                                item.images.edges[0] ? item.images.edges[0].node.originalSrc : ''
+                                            }
+                                            alt = {
+                                                item.images.edges[0] ? item.images.edges[0].node.altText : ''
+                                            }
+                                        />
+                                    );
+                                    const price = item.variants.edges[0].node.price;
+                                    return (
+                                        <ResourceItem
+                                            id={item.id}
+                                            media={media}
+                                            accessibilityLabel={'View details for ${item.title}'}
+                                        >
+                                            <Stack>
+                                                <Stack.Item fill>
+                                                    <h2>
+                                                        <TextStyle variation = "strong">
+                                                            <DisplayText size="small">
+                                                                {item.title}
+                                                            </DisplayText>
+                                                        </TextStyle>
+                                                    </h2>
+                                                </Stack.Item>
+                                                <Stack.Item fill>
+                                                    <DisplayText size="small">
+                                                        ${price}
+                                                    </DisplayText>
+                                                </Stack.Item>
+                                            </Stack>
+                                        </ResourceItem>
+                                    );
+                                }}
+                            />
                         </Card>
                     );
                 }}
